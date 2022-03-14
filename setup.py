2f3dca7a31b41ed7863d40e6d5fc8570973ba4b8
scmtestadf <vanschoot@scm.com>
2022-03-14 17:55:37 +0100

    Automatic Sync from SCM-svn to GitHub syncing the following commits: NestorAguirre: Bug fixed. natsort as a new dependency for PLAMS -- SO--

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
     ],
     keywords         = ['molecular modeling', 'computational chemistry', 'workflow', 'python interface'],
     python_requires  = '>=3.6',
-    install_requires = ['dill>=0.2.4', 'numpy'],
+    install_requires = ['dill>=0.2.4', 'numpy', 'natsort'],
     packages         = packages,
     package_dir      = {'scm.plams': '.'},
     package_data     = {'scm.plams' : ['plams_defaults', 'examples/*']},
