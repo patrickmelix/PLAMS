cc9a3ac2c14fb92684bfb049922cd7bb4a2f9498
scmtestadf <vanschoot@scm.com>
2022-05-06 18:55:45 +0200

    Automatic Sync from SCM-svn to GitHub syncing the following commits: LaurensGroot: Merge back params Windows fixes SO--- SCMSUITE-7873

--- tools/converters.py
+++ tools/converters.py
@@ -109,10 +109,13 @@ def file_to_traj(outfile, trajfile):
     trajfile : str
         will be created
     """
+    from ase.io import read, write
     if os.path.exists(trajfile):
         os.remove(trajfile)
-    cmd = ['sh', os.path.join(os.environ.get('AMSBIN'), 'amspython'), '-m', 'ase', 'convert', outfile, trajfile]
-    saferun(cmd)
+
+    atoms = read(outfile, ':') 
+    write(trajfile, atoms)
+
     if not os.path.exists(trajfile):
         raise RuntimeError("Couldn't write {}".format(trajfile))
 
@@ -208,7 +211,7 @@ def vasp_output_to_ams(vasp_folder, wdir=None, overwrite=False, write_engine_rkf
         return wdir
 
     # convert OUTCAR to a .traj file inside wdir
-    trajfile = file_to_traj(outcar, os.path.join(wdir, 'outcar.traj'))
+    trajfile = file_to_traj(outcar, os.path.join(wdir, 'vasp.traj'))
 
     # remove the target files first if overwrite
     kffile = os.path.join(wdir, 'ams.rkf')
