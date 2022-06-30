cc9527da8eb21aadc2b1e9a34a19c8515e6cceac
scmtestadf <vanschoot@scm.com>
2022-06-30 16:15:35 +0200

    Automatic Sync from SCM-svn to GitHub syncing the following commits: MattiHellstrom: add 1 more example to ASECalculator example SCMSUITE-- SO--

--- examples/ASECalculator.py
+++ examples/ASECalculator.py
@@ -28,7 +28,7 @@ def singlepoint():
     settings = get_settings()
     atoms = get_atoms()
     atoms.calc = AMSCalculator(settings=settings, name='SinglePoint')
-    print("Singlepint")
+    print("Singlepoint through the ASE calculator")
     print("Energy (eV):")
     print(atoms.get_potential_energy())
     print("Forces (eV/ang):")
@@ -36,23 +36,35 @@ def singlepoint():
     print("Stress (eV/ang^3):")
     print(atoms.get_stress())
 
-def geoopt():
-    print("Geo opt in normal mode: One results dir is saved for every step")
-    settings = get_settings()
+def ams_geoopt():
+    print("AMS geo opt run with the ASE calculator")
+    settings = Settings()
+    settings.input.ams.Task = 'GeometryOptimization'
+    settings.input.ams.GeometryOptimization.Convergence.Gradients = 0.01 # hartree/ang
+    settings.input.ForceField.Type = 'UFF'
+    settings.runscript.nproc = 1
     atoms = get_atoms()
-    atoms.calc = AMSCalculator(settings=settings, name='GeoOpt')
-    dyn = BFGS(atoms)
-    dyn.run(fmax=0.05)
+    atoms.calc = AMSCalculator(settings=settings, name='AMS_GeoOpt')
     print("Optimized energy (eV):")
     print(atoms.get_potential_energy())
 
-def worker_geoopt():
-    print("Geo opt in AMSWorker mode: no output files are saved, minimal overhead")
+def ase_geoopt():
+    print("ASE geo opt (ase.optimize.BGFGS) in normal mode: One results dir is saved for every step")
+    settings = get_settings()
+    atoms = get_atoms()
+    atoms.calc = AMSCalculator(settings=settings, name='ASE_GeoOpt')
+    dyn = BFGS(atoms)
+    dyn.run(fmax=0.27)
+    print("Optimized energy (eV):")
+    print(atoms.get_potential_energy())
+
+def ase_geoopt_workermode():
+    print("ASE geo opt (ase.optimize.FGS) in AMSWorker mode: no output files are saved, minimal overhead")
     settings = get_settings(properties=False) #in AMSWorker mode, do not specify the Properties in the settings.
     atoms = get_atoms()
-    atoms.calc = AMSCalculator(settings=settings, name='WorkerGeoOpt', amsworker=True)
+    atoms.calc = AMSCalculator(settings=settings, name='ASE_WorkerGeoOpt', amsworker=True)
     dyn = BFGS(atoms)
-    dyn.run(fmax=0.05)
+    dyn.run(fmax=0.27)
     print("Optimized energy (eV):")
     print(atoms.get_potential_energy())
 
@@ -60,8 +72,9 @@ def worker_geoopt():
 def main():
     init()
     singlepoint()
-    geoopt()
-    worker_geoopt()
+    ams_geoopt()
+    ase_geoopt()
+    ase_geoopt_workermode()
     finish()
 
 if __name__ == '__main__':
