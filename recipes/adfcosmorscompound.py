0d10de489ea2829c02102dbb9db14be163d9ba99
scmtestadf <vanschoot@scm.com>
2022-08-24 11:50:35 +0200

    Automatic Sync from SCM-svn to GitHub syncing the following commits: MattiHellstrom: initial ADFCOSMORSCompound recipe in PLAMS SCMSUITE-8203 SO107

---
+++ recipes/adfcosmorscompound.py
@@ -0,0 +1,189 @@
+import os
+import shutil
+from scm.plams import Settings, JobError, AMSJob, CRSJob, Molecule, AMSResults, CRSResults, KFFile
+from collections import OrderedDict
+from ..core.functions import add_to_instance
+from ..core.basejob import MultiJob
+from ..core.results import Results
+from ..core.settings import Settings
+from ..mol.molecule import Molecule
+from ..mol.atom import Atom
+from ..interfaces.adfsuite.ams import AMSJob
+from ..tools.units import Units
+from ..interfaces.adfsuite.quickjobs import model_to_settings
+import numpy as np
+
+__all__ = ['ADFCOSMORSCompoundJob', 'ADFCOSMORSCompoundResults']
+
+class ADFCOSMORSCompoundResults(Results):
+    """Results class for ADFCOSMORSCompoundJob
+    """
+    def coskfpath(self):
+        """
+            Returns the path to the resulting .coskf
+        """
+
+        return os.path.join(self.job.path, self.job.name+".coskf")
+
+    def get_main_molecule(self):
+        """
+            Returns the optimized molecule
+        """
+
+        return self.job.children['solv'].results.get_main_molecule()
+
+    def get_input_molecule(self):
+        """
+            Returns the input molecule
+        """
+        return self.job.children['gas'].results.get_input_molecule()
+
+
+
+class ADFCOSMORSCompoundJob(MultiJob):
+    """A class for performing the equivalent of Task COSMO-RS Compound in the AMS GUI
+    """
+
+    _result_type = ADFCOSMORSCompoundResults
+
+    def __init__(self, molecule:Molecule, preoptimization=None, **kwargs):
+        """
+
+            Class for running the equivalent of "COSMO-RS Compound" in the AMS
+            GUI. Note that these are ADF calculations, not COSMO-RS
+            calculations!
+
+            Initialize two or three jobs:
+
+            (optional): Preoptimization
+            1. Gasphase optimization
+            2. Take optimized structure and run singlepoint with implicit solvation
+
+            Access the result .coskf file with ``job.results.coskfpath()``.
+            Note: this file will be called jobname.coskf, where jobname is the
+            name of the ADFCOSMORSCompoundJob.
+
+            molecule  : Molecule
+                The initial structure
+
+            preoptimization : str or None
+                If None, do not preoptimize with a fast engine (then initial optimization is done with ADF). Otherwise, can be one of 'UFF', 'GAFF', 'GFNFF', 'GFN1-xTB', 'ANI-2x'. Note that you need valid licenses for ForceField or DFTB or MLPotential to use these preoptimizers.
+
+            Usage:
+
+            .. code-block:: python
+                
+                mol = from_smiles('O')
+                job = ADFCOSMORSCompoundJob(name='water', molecule=mol, preoptimization='UFF')
+                job.run()
+                print(job.results.coskfpath())
+
+        """
+        MultiJob.__init__(self, children=OrderedDict(), **kwargs)
+
+        if preoptimization:
+            preoptimization_s = Settings()
+            preoptimization_s.runscript.nproc = 1
+            preoptimization_s.input.ams.Task = 'GeometryOptimization'
+            preoptimization_s += model_to_settings(preoptimization)
+            preoptimization_job = AMSJob(settings=preoptimization_s, name='preoptimization', molecule=molecule)
+            self.children['preoptimization'] = preoptimization_job
+
+        gas_s = Settings()
+        gas_s.input.ams.Task = 'GeometryOptimization'
+        gas_s += self.adf_settings(solvation=False)
+        gas_job = AMSJob(settings=gas_s, name='gas', molecule=molecule)
+
+        if preoptimization:
+            @add_to_instance(gas_job)
+            def prerun(self):
+                self.molecule = self.parent.children['preoptimization'].results.get_main_molecule()
+
+        self.children['gas'] = gas_job
+
+        solv_s = Settings()
+        solv_s.input.ams.Task = 'SinglePoint'
+        solv_s.input.ams.EngineRestart = 'gasphase-adf.rkf'
+        solv_s.input.ams.LoadSystem.File = 'gasphase-ams.rkf'
+        solv_s += self.adf_settings(solvation=True)
+        solv_job = AMSJob(settings=solv_s, name='solv')
+
+        @add_to_instance(solv_job)
+        def prerun(self):
+            shutil.copyfile(gas_job.results.rkfpath(file='ams'), os.path.join(self.path, 'gasphase-ams.rkf'))
+            shutil.copyfile(gas_job.results.rkfpath(file='adf'), os.path.join(self.path, 'gasphase-adf.rkf'))
+
+        @add_to_instance(solv_job)
+        def postrun(self):
+            self.parent.convert_to_coskf(self.results.rkfpath(file='adf'), os.path.join(self.parent.path, self.parent.name+'.coskf'))
+
+        self.children['solv'] = solv_job
+
+    @staticmethod
+    def adf_settings(solvation:bool) -> Settings:
+        """
+        Returns ADF settings with or without solvation
+
+        If solvation == True, then also include the solvation block.
+        """
+        
+        s = """
+        Engine ADF
+            Basis
+                Type TZP
+                Core Small
+            End
+            XC
+                GGA BP86
+            End
+            Symmetry NOSYM
+        """
+        if solvation:
+            s += """
+
+            Solvation
+                Radii
+                    Br       2.16
+                    C        2.0
+                    Cl       2.05
+                    F        1.72
+                    H        1.3
+                    I        2.32
+                    N        1.83
+                    O        1.72
+                    P        2.13
+                    S        2.16
+                    Si       2.48
+                End
+                Surf Delley
+                Solv name=CRS cav0=0.0 cav1=0.0
+                Charged method=CONJ corr
+                C-Mat EXACT
+                SCF VAR ALL
+                CSMRSP
+            End
+
+            """
+        s += """
+
+            BeckeGrid
+                Quality Good
+            End
+        EndEngine
+        """
+
+        s = AMSJob.from_input(s).settings
+
+        return s
+
+    @staticmethod
+    def convert_to_coskf(rkf: str, coskf: str):
+        """ rkf: absolute path to adf.rkf, coskf: path to write out the resulting .coskf file  """
+        f = KFFile(rkf)
+        cosmo = f.read_section("COSMO")
+        coskf = KFFile(coskf)
+        for k,v in cosmo.items():
+            coskf.write("COSMO",k,v)
+
+
+
