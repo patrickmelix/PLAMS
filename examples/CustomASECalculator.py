e1b67d271a4a6bc36b2b790fb8a7f2fa118fd51b
scmtestadf <vanschoot@scm.com>
2023-01-13 12:15:47 +0100

    Automatic Sync from SCM-svn to GitHub syncing the following commits: PaulSpiering: plams CustomASECalculator example now uses engine ASE. SCMSUITE-8340

--- examples/CustomASECalculator.py
+++ examples/CustomASECalculator.py
@@ -3,14 +3,12 @@ from scm.plams import *
 from ase.calculators.emt import EMT
 import os
 
-def get_calculator(params_path, **kwargs):
+def get_calculator():
     """ 
-    Return an ASE calculator for use with the MLPotential%Backend = ASE. 
+    Return an ASE calculator for use with Engine ASE. 
     
     The function must be called "get_calculator" 
 
-    ``params_path`` is a string corresponding to the MLPotential%ParameterFile input (ignored in this case)
-    
     """
     return EMT()
 
@@ -24,15 +22,14 @@ def main():
     s.runscript.nproc = 1
     s.input.ams.task = 'GeometryOptimization'
     s.input.ams.GeometryOptimization.Convergence.Gradients = 0.01 # hartree/ang
-    s.input.MLPotential.backend = 'ASE'
-    s.input.MLPotential.ASECalculatorFile = os.path.abspath(__file__)   # get it from the current file's get_calculator() method
-    #s.input.MLPotential.ParameterFile = os.path.abspath('some-file.txt') # pass a path to a parameter file as the params_path argument in get_calculator(), not done in this example
+    s.input.ASE.File = os.path.abspath(__file__)   # get it from the current file's get_calculator() method
+    #s.input.ASE.Arguments._1 = "my_argument = my_value" to call get_calculator(my_argument = my_value)
 
     job = AMSJob(settings=s, molecule=mol, name='ams_with_custom_ase_calculator')
     job.run()
 
     energy = job.results.get_energy(unit='eV')
-    print(f"AMS with custom ASE calculator (MLPotential Backend%ASE), EMT potential: final energy {energy:.3f} eV")
+    print(f"AMS with custom ASE calculator (Engine ASE), EMT potential: final energy {energy:.3f} eV")
 
     finish()
 
