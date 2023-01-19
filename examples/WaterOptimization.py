69150b1be292a26542ca52307ef7c1f01007708e
scmtestadf <vanschoot@scm.com>
2023-01-19 11:45:50 +0100

    Automatic Sync from SCM-svn to GitHub syncing the following commits: MattiHellstrom: more properties in PLAMS getting started SCMSUITE-- SO--

--- examples/WaterOptimization.py
+++ examples/WaterOptimization.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 # Perform a geometry optimization of a water molecule and compute
 # the vibrational normal modes using GFN1-xTB. If you do not have
 # a DFTB license, remove the line with DFTB settings and instead set
@@ -18,8 +20,26 @@ settings.input.dftb.Model = 'GFN1-xTB'
 job = AMSJob(molecule=molecule, settings=settings, name='water_optimization')
 result = job.run()
 
+# Calculation timing
+
+timings = result.get_timings()
+
+# Results
+
 energy = result.get_energy(unit='kcal/mol')
+
+homo = result.get_homo_energies(unit='eV')[0]
+lumo = result.get_lumo_energies(unit='eV')[0]
+
+homo_lumo_gap = result.get_smallest_HOMO_LUMO_gap(unit='eV')
+
+dipole_moment = np.linalg.norm(np.array(job.results.get_dipolemoment()))
+dipole_moment *= Units.convert(1.0, 'au', 'debye')
+
 frequencies = result.get_frequencies(unit='cm^-1')
+
+# Optimized structure
+
 optimized_molecule = result.get_main_molecule()
 
 # Unlike python lists, where the index of the first element is 0, 
@@ -29,6 +49,14 @@ bond_angle = optimized_molecule[1].angle(optimized_molecule[2], optimized_molecu
 print('== Results ==')
 print('Optimized geometry:')
 print(optimized_molecule)
-print('Energy      : {:.3f} kcal/mol'.format(energy))
+print('')
 print('Bond angle  : {:.1f} degrees'.format(Units.convert(bond_angle, 'rad', 'degree')))
+print('')
+print('Calculation time: {:.3f} s'.format(timings['elapsed']))
+print('')
+print('Energy      : {:.3f} kcal/mol'.format(energy))
+print('HOMO        : {:.3f} eV'.format(homo))
+print('LUMO        : {:.3f} eV'.format(lumo))
+print('HOMO-LUMO gap : {:.3f} eV'.format(homo_lumo_gap))
+print('Dipole moment: {:.3f} debye'.format(dipole_moment))
 print('Frequencies : {} cm^-1'.format(frequencies))
