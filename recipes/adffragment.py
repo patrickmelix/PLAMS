609102213dc5d17ebee5d42ee25db45b75ddb436
Patrick Melix <patrick.melix@uni-leipzig.de>
2024-08-26 10:24:23 +0200

    Update results collector of ADFFragmentResults, now get the full eda results

--- recipes/adffragment.py
+++ recipes/adffragment.py
@@ -2,13 +2,16 @@ from scm.plams.core.basejob import MultiJob
 from scm.plams.core.results import Results
 from scm.plams.core.settings import Settings
 from scm.plams.interfaces.adfsuite.ams import AMSJob
+from scm.plams.tools.units import Units
+from scm.plams.core.errors import FileError
 from scm.plams.mol.molecule import Molecule
 from scm.plams import add_to_instance
 
+
 from os.path import join as opj
-from os.path import relpath
+from os.path import abspath, isdir, basename, relpath
 from os import symlink
-from typing import List, Union
+from typing import Dict, List, Union
 
 __all__ = ["ADFFragmentJob", "ADFFragmentResults"]
 
@@ -35,12 +38,46 @@ class ADFFragmentResults(Results):
         """Redirect to |ADFResults| of the full calculation."""
         return self.job.full.results.get_dipole_vector(unit)
 
-    def get_energy_decomposition(self):
-        """Get the energy decomposition of the fragment calculation."""
+    def get_energy_decomposition(self, unit='kJ/mol') -> Dict[str, float]:
+        """Get the energy decomposition of the fragment calculation.
+        
+        Args:
+            unit (str, optional): The unit of the energy. Defaults to 'kJ/mol'.
+            
+        Returns:
+            Dict[str, float]: The energy decomposition.
+        """
         energy_section = self.job.full.results.read_rkf_section("Energy", file="adf")
         ret = {}
         for k in ["Electrostatic Energy", "Kinetic Energy", "Elstat Interaction", "XC Energy"]:
-            ret[k] = energy_section[k]
+            ret[k] = Units.convert(energy_section[k], 'au', unit)
+    
+        # most information not available from the KF file
+        res=self.job.full.results
+        res1=self.job.f1.results
+        res2=self.job.f2.results
+        pos = -4  # position of the energy in au the output
+        # E_int appears in a comment below the PEDA Table of the output
+        ret['E_int'] = Units.convert(float(res.grep_output('Total Bonding Energy:')[-2].split()[pos]), 'au', unit)
+        ret['E_int_disp'] = Units.convert(float(res.grep_output('Dispersion Energy:')[-1].split()[pos]), 'au', unit)
+        ret['E_Pauli'] = Units.convert(float(res.grep_output('Pauli Repulsion (Delta')[-1].split()[pos]), 'au', unit)
+        ret['E_elstat'] = Units.convert(float(res.grep_output('Electrostatic Interaction:')[-1].split()[pos]), 'au', unit)
+        ret['E_orb'] = Units.convert(float(res.grep_output('Total Orbital Interactions:')[-1].split()[pos]), 'au', unit)
+
+        ret['E_1'] = res1.get_energy(unit=unit)
+        ret['E_2'] = res2.get_energy(unit=unit)
+
+        if hasattr(self.job, 'f1_opt'):
+            ret['E_1_opt'] = self.job.f1_opt.results.get_energy(unit=unit)
+            ret['E_prep_1'] = ret['E_1'] - ret['E_1_opt']
+        if hasattr(self.job, 'f2_opt'):
+            ret['E_2_opt'] = self.job.f2_opt.results.get_energy(unit=unit)
+            ret['E_prep_2'] = ret['E_2'] - ret['E_2_opt']
+
+        if ('E_1_opt' in ret) and ('E_2_opt' in ret):
+            ret['E_prep'] = ret['E_prep_1'] + ret['E_prep_2']
+            ret['E_bond'] = ret['E_int'] + ret['E_prep']
+        
         return ret
 
 
@@ -98,3 +135,30 @@ class ADFFragmentJob(MultiJob):
 
             return [self.full]
 
+    @classmethod
+    def load_external(cls, path: str, jobname: str=None) -> "ADFFragmentJob":
+        """Load the results of the ADFFragmentJob job from an external path.
+        
+        Args:
+            path (str): The path to the job. It should at least have the
+                        subfolders 'frag1', 'frag2' and 'full'.
+            jobname (str, optional): The name of the job. Defaults to None.
+
+        Returns:
+            ADFFragmentJob: The job with the loaded results.
+        """
+        if not isdir(path):
+            raise FileError("Path {} does not exist, cannot load from it.".format(path))
+        path = abspath(path)
+        jobname = basename(path) if jobname is None else str(jobname)
+
+        job = cls(name=jobname)
+        job.path = path
+        job.status = "copied"
+
+        job.f1 = AMSJob.load_external(opj(path, 'frag1'))
+        job.f2 = AMSJob.load_external(opj(path, 'frag2'))
+        job.full = AMSJob.load_external(opj(path, 'full'))
+        job.children = [job.f1, job.f2, job.full]
+
+        return job
