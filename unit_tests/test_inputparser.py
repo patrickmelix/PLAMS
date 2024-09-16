87d58973a88686065a40697f697e2c6c3992099d
David Ormrod Morley <ormrodmorley@scm.com>
2024-09-16 10:09:25 +0200

    Cache libbase input parser in input parser facade SCMSUITE-10016 SO107

--- unit_tests/test_inputparser.py
+++ unit_tests/test_inputparser.py
@@ -1,6 +1,7 @@
 import pytest
 import builtins
 from importlib import reload
+from threading import Thread
 
 from scm.plams.core.settings import Settings
 from scm.plams.unit_tests.test_helpers import get_mock_import_function, skip_if_no_ams_installation
@@ -34,7 +35,44 @@ EndEngine
     geometry_optimisation_settings.ADF.XC.GGA = "PBE"
     geometry_optimisation_settings.ams.Task = "GeometryOptimization"
 
-    yield [(geometry_optimisation_input, geometry_optimisation_settings)]
+    md_input = """
+MolecularDynamics
+  Checkpoint
+    Frequency 1000000
+  End
+  InitialVelocities
+    Temperature 600.0
+  End
+  NSteps 100000
+  Shake
+    All Bonds H *
+  End
+  Thermostat
+    Tau 100
+    Temperature 600.0
+    Type Berendsen
+  End
+  TimeStep 1.0
+  Trajectory
+    SamplingFreq 100
+  End
+End
+"""
+
+    md_settings = Settings()
+    md_thermo_settings = Settings()
+    md_settings.ams.MolecularDynamics.Checkpoint.Frequency = "1000000"
+    md_settings.ams.MolecularDynamics.InitialVelocities.Temperature = "600.0"
+    md_settings.ams.MolecularDynamics.NSteps = "100000"
+    md_settings.ams.MolecularDynamics.Shake.All = ["Bonds H *"]
+    md_thermo_settings.Tau = "100"
+    md_thermo_settings.Temperature = "600.0"
+    md_thermo_settings.Type = "Berendsen"
+    md_settings.ams.MolecularDynamics.Thermostat = [md_thermo_settings]
+    md_settings.ams.MolecularDynamics.TimeStep = "1.0"
+    md_settings.ams.MolecularDynamics.Trajectory.SamplingFreq = "100"
+
+    yield [(geometry_optimisation_input, geometry_optimisation_settings), (md_input, md_settings)]
 
     # Tear-down: reload module without the patched import function
     import scm.plams.interfaces.adfsuite.inputparser as inputparser
@@ -58,21 +96,72 @@ System
 End
     """
 
-    water_atoms_settings = Settings()
-    water_atoms_settings.Atoms._1 = [
-        "O       0.0000000000       " "0.0000000000       0.0000000000",
-        "H       1.0000000000       " "0.0000000000       0.0000000000",
-        "H       0.0000000000       " "1.0000000000       0.0000000000",
+    water_settings = Settings()
+    water_settings.Atoms._1 = [
+        "O       0.0000000000       0.0000000000       0.0000000000",
+        "H       1.0000000000       0.0000000000       0.0000000000",
+        "H       0.0000000000       1.0000000000       0.0000000000",
     ]
     water_system_settings = Settings()
-    water_system_settings.System = [water_atoms_settings]
+    water_system_settings.System = [water_settings]
 
-    yield [(water_system_input, water_system_settings)]
+    graphene_system_input = """
+System
+   Atoms
+      C   -1.2300000000000000 -2.1304224999999999  0.0000000000000000
+      C   -1.2300000000000000 -0.7101408299999999  0.0000000000000000
+      C    0.0000000000000000  0.0000000000000000  0.0000000000000000
+      C    0.0000000000000000  1.4202816700000001  0.0000000000000000
+   End
+   Lattice
+        2.4600000000000000   0.0000000000000000   0.0000000000000000
+        0.0000000000000000   4.2608449999999998   0.0000000000000000
+        0.0000000000000000   0.0000000000000000  80.0000000000000000
+   End
+End
+"""
+    graphene_settings = Settings()
+    graphene_settings.Atoms._1 = [
+        "C   -1.2300000000000000 -2.1304224999999999  0.0000000000000000",
+        "C   -1.2300000000000000 -0.7101408299999999  0.0000000000000000",
+        "C    0.0000000000000000  0.0000000000000000  0.0000000000000000",
+        "C    0.0000000000000000  1.4202816700000001  0.0000000000000000",
+    ]
+    graphene_settings.Lattice._1 = [
+        "2.4600000000000000   0.0000000000000000   0.0000000000000000",
+        "0.0000000000000000   4.2608449999999998   0.0000000000000000",
+        "0.0000000000000000   0.0000000000000000  80.0000000000000000",
+    ]
+    graphene_system_settings = Settings()
+    graphene_system_settings.System = [graphene_settings]
+
+    yield [(water_system_input, water_system_settings), (graphene_system_input, graphene_system_settings)]
 
     # Tear-down: reload module without the patched import function
     import scm.plams.interfaces.adfsuite.inputparser as inputparser
 
     reload(inputparser)
+
+
+@pytest.fixture
+def large_system_input():
+    """
+    Input with large molecule.
+    """
+    large_system_input = """
+    System
+       Atoms
+    """
+    for i in range(100):
+        for j in range(100):
+            large_system_input += f"""        C    {i}    {j}    0
+"""
+    large_system_input += """
+  End
+End
+"""
+
+    return large_system_input
 
 
 def test_to_settings_without_scmlibbase_succeeds(ams_text_inputs, monkeypatch):
@@ -145,3 +234,36 @@ def to_dict_succeeds(input_texts, input_parser):
     for input_text, expected_settings in input_texts:
         actual_dict = input_parser.to_dict("ams", input_text)
         assert actual_dict == expected_settings.as_dict()
+
+
+def test_libbase_parser_cached(ams_text_inputs):
+    input_parser = get_input_parser_or_skip()
+
+    assert "ams" not in input_parser.parser.parsers.keys()
+
+    input_parser.to_dict("ams", ams_text_inputs[0][0])
+
+    assert "ams" in input_parser.parser.parsers.keys()
+
+    assert input_parser.parser == input_parser.parser
+
+
+def test_thread_safe(large_system_input):
+    get_input_parser_or_skip()
+
+    errors = []
+
+    def parse():
+        try:
+            input_parser = get_input_parser_or_skip()
+            input_parser.to_settings("ams", large_system_input)
+        except Exception as e:
+            errors.append(e)
+
+    threads = [Thread(target=parse) for _ in range(100)]
+    for t in threads:
+        t.start()
+    for t in threads:
+        t.join()
+
+    assert len(errors) == 0
