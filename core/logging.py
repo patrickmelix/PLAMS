c899fda577d07dd70b1c7ac88682533d753c9a13
David Ormrod Morley <ormrodmorley@scm.com>
2024-10-17 15:20:28 +0200

    Add lock into logger SCMSUITE-9740 SO107

--- core/logging.py
+++ core/logging.py
@@ -1,6 +1,7 @@
 import logging
 import sys
 from typing import Optional
+import threading
 
 
 from scm.plams.core.errors import FileError
@@ -44,6 +45,7 @@ class Logger:
         self._stdout_handler: Optional[logging.StreamHandler] = None
         self._file_handler: Optional[logging.FileHandler] = None
         self._formatter: Optional[logging.Formatter] = None
+        self._lock = threading.Lock()
 
     def configure_stdout(self, level: int) -> None:
         """
@@ -53,13 +55,14 @@ class Logger:
         Note that this is only a PLAMS logging level, it will be mapped to a level between INFO and WARNING for the
         python logger.
         """
-        if self._stdout_handler is None:
-            self._stdout_handler = logging.StreamHandler(sys.stdout)
-            self._stdout_handler.setFormatter(self._formatter)
-            self._logger.addHandler(self._stdout_handler)
+        with self._lock:
+            if self._stdout_handler is None:
+                self._stdout_handler = logging.StreamHandler(sys.stdout)
+                self._stdout_handler.setFormatter(self._formatter)
+                self._logger.addHandler(self._stdout_handler)
 
-        if level != self._stdout_handler.level:
-            self._stdout_handler.setLevel(28 - level)
+            if level != self._stdout_handler.level:
+                self._stdout_handler.setLevel(28 - level)
 
     def configure_logfile(self, path: Optional[str], level: int = 0) -> None:
         """
@@ -72,25 +75,27 @@ class Logger:
         Note that this is only a PLAMS logging level, it will be mapped to a level between INFO and WARNING for the
         python logger.
         """
-        # Remove and close existing file handler if present and required
-        if self._file_handler is not None and (path is None or path != self._file_handler.baseFilename):
-            self._logger.removeHandler(self._file_handler)
-            self._file_handler.close()
+        with self._lock:
+            # Remove and close existing file handler if present and required
+            if self._file_handler is not None and (path is None or path != self._file_handler.baseFilename):
+                self._logger.removeHandler(self._file_handler)
+                self._file_handler.flush()
+                self._file_handler.close()
 
-        # Add new file handler if required
-        if path is not None and (self._file_handler is None or path != self._file_handler.baseFilename):
+            # Add new file handler if required
+            if path is not None and (self._file_handler is None or path != self._file_handler.baseFilename):
 
-            # Check logfile is not already in use by another logger
-            for name, logger in LoggerManager._loggers.items():
-                if logger._file_handler is not None and logger._file_handler.baseFilename == path:
-                    raise FileError(f"Logger '{name}' already exists with path '{path}'")
+                # Check logfile is not already in use by another logger
+                for name, logger in LoggerManager._loggers.items():
+                    if logger._file_handler is not None and logger._file_handler.baseFilename == path:
+                        raise FileError(f"Logger '{name}' already exists with path '{path}'")
 
-            self._file_handler = logging.FileHandler(path)
-            self._file_handler.setFormatter(self._formatter)
-            self._logger.addHandler(self._file_handler)
+                self._file_handler = logging.FileHandler(path)
+                self._file_handler.setFormatter(self._formatter)
+                self._logger.addHandler(self._file_handler)
 
-        if self._file_handler is not None:
-            self._file_handler.setLevel(28 - level)
+            if self._file_handler is not None:
+                self._file_handler.setLevel(28 - level)
 
     def configure_formatter(self, include_date: bool, include_time: bool) -> None:
         """
@@ -105,16 +110,21 @@ class Logger:
         elif include_time:
             datefmt = "[%H:%M:%S]"
 
-        if self._formatter is None or datefmt != self._formatter.datefmt:
-            fmt = "%(asctime)s %(message)s" if datefmt is not None else None
-            self._formatter = logging.Formatter(fmt, datefmt=datefmt)
-            if self._stdout_handler is not None:
-                self._stdout_handler.setFormatter(self._formatter)
-            if self._file_handler is not None:
-                self._file_handler.setFormatter(self._formatter)
+        with self._lock:
+            if self._formatter is None or datefmt != self._formatter.datefmt:
+                fmt = "%(asctime)s %(message)s" if datefmt is not None else None
+                self._formatter = logging.Formatter(fmt, datefmt=datefmt)
+                if self._stdout_handler is not None:
+                    self._stdout_handler.setFormatter(self._formatter)
+                if self._file_handler is not None:
+                    self._file_handler.setFormatter(self._formatter)
 
     def log(self, message: str, level: int) -> None:
         """
         Log a message with the given level of verbosity.
         """
-        self._logger.log(28 - level, message)
+        # Shouldn't really have to take a lock here as logging itself is thread-safe
+        # but in the PLAMS log function the logfile can be reconfigured on a per-call basis
+        # which could easily lead to dropped logs if this were multi-threaded.
+        with self._lock:
+            self._logger.log(28 - level, message)
