97ce05a4db4988f73fb6fd7109ef1d91e074aadb
Franco Egidi <egidi@scm.com>
2025-01-08 18:46:52 +0100

    I run black on my files

--- examples/SerenityInterface_examples/hf_unres.py
+++ examples/SerenityInterface_examples/hf_unres.py
@@ -1,20 +1,21 @@
 from scm.plams import *
-init(folder='test_methylradical')
+
+init(folder="test_methylradical")
 
 sersett = SerenitySettings()
-#The path to the geometry file
-sersett.input.system.MethylRadical.geometry = 'methylRadical.xyz'
-#You can define the excess number of alpha electrons here
-sersett.input.system.MethylRadical.spin = '1'
-#This means that we allow singly occupied orbitals which leads to different
-#spatial-orbitals for alpha and beta electrons.
-#Serenity will choose this automatically as soon as spin != 0.
-sersett.input.system.MethylRadical.scfMode = 'UNRESTRICTED'
-sersett.input.system.MethylRadical.basis.label = 'Def2-SVP'
-#Performs a SCF calculation for the given active (act) system
-sersett.input.task.SCF.act = 'MethylRadical'
+# The path to the geometry file
+sersett.input.system.MethylRadical.geometry = "methylRadical.xyz"
+# You can define the excess number of alpha electrons here
+sersett.input.system.MethylRadical.spin = "1"
+# This means that we allow singly occupied orbitals which leads to different
+# spatial-orbitals for alpha and beta electrons.
+# Serenity will choose this automatically as soon as spin != 0.
+sersett.input.system.MethylRadical.scfMode = "UNRESTRICTED"
+sersett.input.system.MethylRadical.basis.label = "Def2-SVP"
+# Performs a SCF calculation for the given active (act) system
+sersett.input.task.SCF.act = "MethylRadical"
 
-serjob = SerenityJob(settings=sersett, name='Serenity_methylradical')
+serjob = SerenityJob(settings=sersett, name="Serenity_methylradical")
 serjob.run()
 
 finish()
