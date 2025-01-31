b2458ad156a785a0acf0b1414b54ba733ab67ea0
David Ormrod Morley <ormrodmorley@scm.com>
2025-01-31 15:20:52 +0100

    Make sure jobmanager _clean does not create a workdir for the first time SO107

--- unit_tests/test_jobmanager.py
+++ unit_tests/test_jobmanager.py
@@ -5,6 +5,7 @@ import uuid
 from scm.plams.core.jobmanager import JobManager
 from scm.plams.core.settings import JobManagerSettings
 from scm.plams.core.errors import PlamsError
+from scm.plams.unit_tests.test_basejob import DummySingleJob
 
 
 class TestJobManager:
@@ -29,6 +30,26 @@ class TestJobManager:
         assert job_manager.workdir == workdir
 
         os.rmdir(job_manager.workdir)
+
+    def test_load_and_clean_do_not_create_workdir(self):
+        # Given job manager
+        folder = str(uuid.uuid4())
+        job_manager = JobManager(settings=JobManagerSettings(), folder=folder)
+
+        # When load job
+        job = DummySingleJob()
+        job.run()
+        job.results.wait()
+        job_manager.load_job(f"{job.path}/{job.name}.dill")
+
+        # Then workdir not created
+        assert not os.path.exists(job_manager._workdir)
+
+        # When clean the jobmanager
+        job_manager._clean()
+
+        # Then workdir not created
+        assert not os.path.exists(job_manager._workdir)
 
     @pytest.mark.parametrize(
         "path_exists,folder_exists,use_existing_folder,expected_workdir",
