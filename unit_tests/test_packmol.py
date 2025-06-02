ee3190d926dd519d8b81e510c0ddd9b0582fdcc6
David Ormrod Morley <ormrodmorley@scm.com>
2025-06-02 15:35:40 +0200

    Add tests for packmol method

--- unit_tests/test_packmol.py
+++ unit_tests/test_packmol.py
@@ -1,10 +1,11 @@
 import pytest
 from dataclasses import dataclass
-from typing import Optional, List
+from typing import Optional, List, Union
 
 from scm.plams.mol.molecule import Molecule
 from scm.plams.interfaces.molecule.rdkit import from_smiles
-from scm.plams.interfaces.molecule.packmol import PackMolStructure
+from scm.plams.interfaces.molecule.packmol import PackMolStructure, packmol
+from scm.plams.unit_tests.test_helpers import skip_if_no_ams_installation
 
 
 class TestPackmolStructure:
@@ -244,3 +245,420 @@ end structure
             input_block = structure.get_input_block("pm", 0.1)
 
             assert input_block == test_case.expected_input
+
+
+class TestPackMol:
+
+    @dataclass
+    class UnhappyTestCase:
+        """
+        Dataclass holding inputs for packmol and the expected error
+        """
+
+        molecules: Union[List[Molecule], Molecule]
+        mole_fractions: Optional[List[float]] = None
+        density: Optional[float] = None
+        n_atoms: Optional[int] = None
+        box_bounds: Optional[List[float]] = None
+        n_molecules: Union[List[int], int, None] = None
+        sphere: bool = False
+        fix_first: bool = False
+        region_names: Union[List[str], str, None] = None
+        expected_error: str = ".*"
+
+    water = from_smiles("O")
+    acetonitrile = from_smiles("CC#N")
+    ammonium = from_smiles("[NH4+]")
+    chloride = from_smiles("[Cl-]")
+
+    unhappy_test_cases = [
+        UnhappyTestCase(molecules=water, expected_error="must specify either n_atoms, n_molecules or density"),
+        UnhappyTestCase(
+            molecules=water,
+            n_atoms=300,
+            n_molecules=100,
+            expected_error="n_atoms and n_molecules are mutually exclusive",
+        ),
+        UnhappyTestCase(
+            molecules=water,
+            n_atoms=300,
+            box_bounds=[0, 0, 0, 5, 5, 5],
+            density=0.9,
+            expected_error="n_atoms, box_bounds and density specified at the same time",
+        ),
+        UnhappyTestCase(
+            molecules=water,
+            n_molecules=100,
+            box_bounds=[0, 0, 0, 5, 5, 5],
+            density=0.9,
+            expected_error="n_molecules, box_bounds and density specified at the same time",
+        ),
+        UnhappyTestCase(
+            molecules=[water, acetonitrile],
+            n_molecules=[100, 100],
+            mole_fractions=[0.5, 0.5],
+            expected_error="mole_fractions and n_molecules are mutually exclusive",
+        ),
+        UnhappyTestCase(
+            molecules=[water, acetonitrile],
+            n_molecules=1,
+            fix_first=True,
+            expected_error="fix_first requires that n_molecules is a list where the first element is 1",
+        ),
+        UnhappyTestCase(
+            molecules=[water, acetonitrile],
+            n_molecules=[42, 1],
+            fix_first=True,
+            expected_error="fix_first requires that n_molecules is a list where the first element is 1",
+        ),
+        UnhappyTestCase(
+            molecules=[water, acetonitrile],
+            n_molecules=42,
+            expected_error="molecules is a list, but n_molecules is not",
+        ),
+        UnhappyTestCase(
+            molecules=[water, acetonitrile], n_molecules=[42], expected_error=r"len\(n_molecules\) != len\(molecules\)"
+        ),
+        UnhappyTestCase(
+            molecules=[water, acetonitrile],
+            density=0.9,
+            mole_fractions=42,
+            expected_error="molecules is a list, but mole_fractions is not",
+        ),
+        UnhappyTestCase(
+            molecules=[water, acetonitrile],
+            density=0.9,
+            mole_fractions=[42],
+            expected_error=r"len\(mole_fractions\) != len\(molecules\)",
+        ),
+        UnhappyTestCase(
+            molecules=[water, acetonitrile],
+            n_molecules=[1, 2],
+            region_names="foo",
+            expected_error="molecules is a list, but region_names is not",
+        ),
+        UnhappyTestCase(
+            molecules=[water, acetonitrile],
+            n_molecules=[1, 2],
+            region_names=["foo"],
+            expected_error=r"len\(region_names\) != len\(molecules\)",
+        ),
+        UnhappyTestCase(
+            molecules=water, n_molecules=[42], expected_error="n_molecules is a list, when molecules is not"
+        ),
+        UnhappyTestCase(
+            molecules=water,
+            density=0.9,
+            mole_fractions=[0.5],
+            expected_error="mole_fractions is a list, when molecules is not",
+        ),
+        UnhappyTestCase(
+            molecules=water,
+            n_molecules=42,
+            region_names=["foo"],
+            expected_error="region_names is a list, when molecules is not",
+        ),
+        UnhappyTestCase(
+            molecules=[water, acetonitrile],
+            n_molecules=[0, 0],
+            expected_error="The sum of n_molecules is 0, which is very close to 0",
+        ),
+        UnhappyTestCase(
+            molecules=[water, acetonitrile],
+            n_molecules=[20, -10],
+            expected_error="All n_molecules must be >= 0",
+        ),
+        UnhappyTestCase(
+            molecules=[water, acetonitrile],
+            density=0.9,
+            mole_fractions=[0, 0],
+            expected_error="The sum of mole fractions is 0, which is very close to 0",
+        ),
+        UnhappyTestCase(
+            molecules=[water, acetonitrile],
+            density=0.9,
+            mole_fractions=[1.0, -0.5],
+            expected_error="All mole fractions must be >= 0",
+        ),
+        UnhappyTestCase(
+            molecules=[water, acetonitrile],
+            density=0.9,
+            mole_fractions=[0.5, 0.5],
+            expected_error="Illegal combination of arguments: n_atoms=None, n_molecules=None, box_bounds=None, density=0.9",
+        ),
+    ]
+
+    @pytest.mark.parametrize("test_case", unhappy_test_cases)
+    def test_pack_mol_unhappy(self, test_case):
+
+        with pytest.raises(ValueError, match=test_case.expected_error):
+            packmol(
+                molecules=test_case.molecules,
+                mole_fractions=test_case.mole_fractions,
+                density=test_case.density,
+                n_atoms=test_case.n_atoms,
+                box_bounds=test_case.box_bounds,
+                n_molecules=test_case.n_molecules,
+                sphere=test_case.sphere,
+                fix_first=test_case.fix_first,
+                region_names=test_case.region_names,
+                executable="no-op",
+            )
+
+    @dataclass
+    class HappyTestCase:
+        """
+        Dataclass holding inputs for packmol and the expected return values
+        """
+
+        molecules: Union[List[Molecule], Molecule]
+        mole_fractions: Optional[List[float]] = None
+        density: Optional[float] = None
+        n_atoms: Optional[int] = None
+        box_bounds: Optional[List[float]] = None
+        n_molecules: Optional[Union[List[int], int]] = None
+        sphere: bool = False
+        fix_first: bool = False
+        keep_bonds: bool = True
+        keep_atom_properties: bool = True
+        region_names: Union[List[str], str, None] = None
+        expected_n_atoms: Optional[int] = None
+        expected_n_molecules: Optional[List[int]] = None
+        expected_mole_fractions: Optional[List[float]] = None
+        expected_volume: Optional[float] = None
+        expected_density: Optional[float] = None
+        expected_radius: Optional[float] = None
+        expected_region_names: Optional[List[str]] = None
+
+    happy_test_cases = [
+        HappyTestCase(
+            water,
+            n_atoms=194,
+            density=1.0,
+            expected_n_atoms=195,
+            expected_n_molecules=[65],
+            expected_mole_fractions=[1.0],
+            expected_volume=1944.4885901260927,
+            expected_density=1.0,
+        ),
+        HappyTestCase(
+            water,
+            density=1.0,
+            box_bounds=[0.0, 0.0, 0.0, 8.0, 12.0, 14.0],
+            expected_n_atoms=135,
+            expected_n_molecules=[45],
+            expected_mole_fractions=[1.0],
+            expected_volume=1344,
+            expected_density=1.0016253039797856,
+        ),
+        HappyTestCase(
+            water,
+            n_molecules=64,
+            density=1.0,
+            expected_n_atoms=192,
+            expected_n_molecules=[64],
+            expected_mole_fractions=[1.0],
+            expected_volume=1914.5733810472298,
+            expected_density=1.0,
+        ),
+        HappyTestCase(
+            water,
+            n_molecules=64,
+            box_bounds=[0.0, 0.0, 0.0, 12.0, 13.0, 14.0],
+            expected_n_atoms=192,
+            expected_n_molecules=[64],
+            expected_mole_fractions=[1.0],
+            expected_volume=2184,
+            expected_density=0.8766361634831642,
+        ),
+        HappyTestCase(
+            [water],
+            n_atoms=100,
+            expected_n_atoms=99,
+            expected_n_molecules=[33],
+            expected_mole_fractions=[1.0],
+            expected_volume=866.5519946802023,
+            expected_density=1.1392298507913536,
+        ),
+        HappyTestCase(
+            [water, acetonitrile],
+            n_atoms=200,
+            mole_fractions=[0.666, 0.334],
+            density=0.92,
+            expected_n_atoms=201,
+            expected_n_molecules=[33, 17],
+            expected_mole_fractions=[0.66, 0.34],
+            expected_volume=2332.679633984687,
+            expected_density=0.92,
+        ),
+        HappyTestCase(
+            [water, acetonitrile],
+            n_atoms=200,
+            mole_fractions=[0.666, 0.334],
+            box_bounds=[0, 0, 0, 13.2, 13.2, 13.2],
+            expected_n_atoms=201,
+            expected_n_molecules=[33, 17],
+            expected_mole_fractions=[0.66, 0.34],
+            expected_volume=2299.968,
+            expected_density=0.9330848356437622,
+        ),
+        HappyTestCase(
+            [water, acetonitrile],
+            n_molecules=[33, 17],
+            density=0.92,
+            expected_n_atoms=201,
+            expected_n_molecules=[33, 17],
+            expected_mole_fractions=[0.66, 0.34],
+            expected_volume=2332.679633984687,
+            expected_density=0.92,
+        ),
+        HappyTestCase(
+            [water, acetonitrile],
+            n_molecules=[33, 17],
+            box_bounds=[0, 0, 0, 13.2, 13.2, 13.2],
+            expected_n_atoms=201,
+            expected_n_molecules=[33, 17],
+            expected_mole_fractions=[0.66, 0.34],
+            expected_volume=2299.968,
+            expected_density=0.9330848356437622,
+        ),
+        HappyTestCase(
+            molecules=[water],
+            n_molecules=[100],
+            density=1.0,
+            sphere=True,
+            expected_n_atoms=300,
+            expected_n_molecules=[100],
+            expected_mole_fractions=[1.0],
+            expected_volume=2991.5209078862968,
+            expected_density=1.0,
+            expected_radius=8.93856517377096,
+        ),
+        HappyTestCase(
+            [water, acetonitrile],
+            n_molecules=[33, 17],
+            sphere=True,
+            expected_n_atoms=201,
+            expected_n_molecules=[33, 17],
+            expected_mole_fractions=[0.66, 0.34],
+            expected_volume=2543.322893286219,
+            expected_density=0.8438036982763867,
+            expected_radius=8.46780724966871,
+        ),
+        HappyTestCase(
+            molecules=[water, ammonium, chloride],
+            n_molecules=[6, 3, 1],
+            density=0.4,
+            sphere=True,
+            expected_n_atoms=34,
+            expected_n_molecules=[6, 3, 1],
+            expected_mole_fractions=[0.6, 0.3, 0.1],
+            expected_volume=820.55525521996,
+            expected_density=0.4,
+            expected_radius=5.807729858286062,
+        ),
+        HappyTestCase(
+            [water, acetonitrile],
+            n_atoms=200,
+            mole_fractions=[0.666, 0.334],
+            density=0.92,
+            keep_bonds=False,
+            expected_n_atoms=201,
+            expected_n_molecules=[33, 17],
+            expected_mole_fractions=[0.66, 0.34],
+            expected_volume=2332.679633984687,
+            expected_density=0.92,
+            expected_region_names=["mol0", "mol0", "mol1", "mol1"],
+        ),
+        HappyTestCase(
+            [water, acetonitrile],
+            n_atoms=200,
+            mole_fractions=[0.666, 0.334],
+            density=0.92,
+            region_names=["water", "acetonitrile"],
+            expected_n_atoms=201,
+            expected_n_molecules=[33, 17],
+            expected_mole_fractions=[0.66, 0.34],
+            expected_volume=2332.679633984687,
+            expected_density=0.92,
+            expected_region_names=["water", "acetonitrile"],
+        ),
+    ]
+
+    @pytest.mark.parametrize("test_case", happy_test_cases)
+    def test_pack_mol_happy_only_details(self, test_case):
+        _, details = packmol(
+            molecules=test_case.molecules,
+            mole_fractions=test_case.mole_fractions,
+            density=test_case.density,
+            n_atoms=test_case.n_atoms,
+            box_bounds=test_case.box_bounds,
+            n_molecules=test_case.n_molecules,
+            sphere=test_case.sphere,
+            fix_first=test_case.fix_first,
+            keep_bonds=test_case.keep_bonds,
+            keep_atom_properties=test_case.keep_atom_properties,
+            region_names=test_case.region_names,
+            executable=".",  # hack, this just needs to be any path that exists, but the exe is not run
+            _return_only_details=True,
+        )
+
+        assert details["n_atoms"] == test_case.expected_n_atoms
+        assert details["n_molecules"] == test_case.expected_n_molecules
+        assert details["mole_fractions"] == test_case.expected_mole_fractions
+
+    @pytest.mark.parametrize("test_case", happy_test_cases)
+    def test_pack_mol_happy(self, test_case):
+        import numpy as np
+
+        skip_if_no_ams_installation()
+
+        mol, details = packmol(
+            molecules=test_case.molecules,
+            mole_fractions=test_case.mole_fractions,
+            density=test_case.density,
+            n_atoms=test_case.n_atoms,
+            box_bounds=test_case.box_bounds,
+            n_molecules=test_case.n_molecules,
+            sphere=test_case.sphere,
+            fix_first=test_case.fix_first,
+            keep_bonds=test_case.keep_bonds,
+            keep_atom_properties=test_case.keep_atom_properties,
+            region_names=test_case.region_names,
+            return_details=True,
+        )
+
+        assert details["n_atoms"] == test_case.expected_n_atoms
+        assert details["n_molecules"] == test_case.expected_n_molecules
+        assert details["mole_fractions"] == test_case.expected_mole_fractions
+        assert np.isclose(details["volume"], test_case.expected_volume)
+        assert np.isclose(details["density"], test_case.expected_density)
+        if test_case.expected_radius:
+            assert np.isclose(details["radius"], test_case.expected_radius)
+        else:
+            assert "radius" not in details
+
+        assert len(mol.atoms) == test_case.expected_n_atoms
+        mols = {}
+        for m in mol.separate():
+            f = m.get_formula()
+            if f not in mols:
+                mols[f] = [m, 1]
+            else:
+                mols[f][1] += 1
+
+        if test_case.keep_bonds:
+            assert [c for _, c in mols.values()] == test_case.expected_n_molecules
+            for m, _ in mols.values():
+                if len(m) > 1:
+                    assert m.bonds
+        else:
+            for m, _ in mols.values():
+                assert not m.bonds
+
+        if test_case.expected_region_names:
+            for i, (m, _) in enumerate(mols.values()):
+                assert all(a.properties.region == {test_case.expected_region_names[i]} for a in m.atoms)
+        else:
+            for i, (m, _) in enumerate(mols.values()):
+                assert all(a.properties.region == {f"mol{i}"} for a in m.atoms)
